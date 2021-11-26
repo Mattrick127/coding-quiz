417a19ade6d5f4e0ecd693a431e64b2cdbba56ec
Sodaman <mattrick2214@gmail.com>
2021-11-26 12:46:34 -0600

    Added timer functionality.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,37 @@
-var timerEl = document.getElementById('time');
+var startQuiz = document.getElementById("start");
+var title = document.getElementById("title");
+var instructions = document.getElementById("instructions");
+var countdownEl = document.getElementById("countdown");
 
-function startTest () {
-    
+var myCountdown;
+
+// function countdown () {
+//     myCountdown = setTimeout(function(){
+//         countdownEl.textContent = ("3 seconds.");
+//     },3000)
+// }
+// Timer that counts down from 5
+function countdown() {
+    var timeLeft = 60;
+  
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        countdownEl.textContent = timeLeft + ' seconds remaining';
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+    }, 1000);
+  }
+console.log (startQuiz)
+console.log (countdownEl)
+
+
+var hideElements = function () {
+    startQuiz.classList.add ("hidden")
+    title.classList.add ("hidden")
+    instructions.classList.add ("hidden")
+countdown()
 }
+
+
+startQuiz.addEventListener ("click",hideElements) 

410fa2d150e83acb2d19bd72a53c7d4c475dab97
Sodaman <mattrick2214@gmail.com>
2021-11-27 13:39:58 -0600

    Added actual first question and javascript question transfer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,36 +2,45 @@ var startQuiz = document.getElementById("start");
 var title = document.getElementById("title");
 var instructions = document.getElementById("instructions");
 var countdownEl = document.getElementById("countdown");
+var section = document.querySelector(".questions");
+var uno = document.querySelector(".uno");
 
 var myCountdown;
-
-// function countdown () {
-//     myCountdown = setTimeout(function(){
-//         countdownEl.textContent = ("3 seconds.");
-//     },3000)
-// }
-// Timer that counts down from 5
 function countdown() {
     var timeLeft = 60;
   
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
-        // Set the `textContent` of `timerEl` to show the remaining seconds
-        countdownEl.textContent = timeLeft + ' seconds remaining';
-        // Decrement `timeLeft` by 1
-        timeLeft--;
+        if (timeLeft > 1) {
+            countdownEl.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
+        } else if (timeLeft === 1) {
+                countdownEl.textContent = timeLeft + ' second remaining';
+                timeLeft--;
+            } else {
+                countdownEl.textContent = '';
+                clearInterval(timeInterval);
+        }
     }, 1000);
   }
-console.log (startQuiz)
-console.log (countdownEl)
-
-
-var hideElements = function () {
+var changeElements = function () {
     startQuiz.classList.add ("hidden")
     title.classList.add ("hidden")
     instructions.classList.add ("hidden")
+    section.classList.remove ("hidden")
+    uno.classList.remove ("hidden")
 countdown()
 }
 
+function quizLogic(e) {
+    if (e.target.matches(".correct")) {
+        console.log ("correct answer")
+    } else {
+        console.log ("wrong answer")
+        countdownEl--;
+    }
+}
+section.addEventListener ("click",quizLogic)
 
-startQuiz.addEventListener ("click",hideElements) 
+
+startQuiz.addEventListener ("click",changeElements) 
